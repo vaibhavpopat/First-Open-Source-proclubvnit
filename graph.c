74a43f4aca91d35beb750109f1f0436b184a4524
ChaityaChheda <34499707+ChaityaChheda@users.noreply.github.com>
2018-07-07 20:51:52 +0530

    create graph.c

---
+++ graph.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <assert.h>	
+#include <time.h>
+
+typedef struct graph{
+
+
+};
+long ShortestPathLength(graph g, ...)
+{
+
+}
+int main()
+{
+    
+    graph G;
+
+    G = readGraph();
+    // returns the shortest path from onr node to another
+    shortestPath = ShortestPathLength(G, ...);
+    //return a graph that is minimum spanning tree;
+    minimumSpanningTree = MinispanningTree(G);
+
+    //cycle detection return boolean, true if cycle exists and false otherwise
+    iscycle = checkCycle(G);
+    //check bipartite graph.
+    isbipartite = checkBipartite(G);
+    //the function should perform the topological sort 
+    TopologicalSort(G);
+
+    //implement BFS DFS
+    bfs(G);
+    dfs(G);
+
+    return 0;
+}
